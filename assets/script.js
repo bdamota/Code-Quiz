18b34413cdacf918e8e28c1316e85a8aeb112526
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-13 19:04:07 -0700

    timer function added

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,32 @@
 
+// Variable Declarations 
+var timerEl = document.getElementById("countdown")
+
+ //The array of questions 
+ var questions = [
+    { q:'The sky is blue.', a: 't' },
+    { q: 'Snow is cold.', a: 't' },
+    { q: 'Water is dry.', a: 'f' }
+];
+
+// Timer Function 
+function prepareGame() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function() {
+        timerEl.textContent = "Time:" + timeLeft + "s";
+        timeLeft--;
+
+        if (timeLeft === 0) {
+           timerEl.textContent = "";
+           clearInterval(timeInterval);
+           output();
+        }
+
+    }, 1000);
+}
+
+prepareGame();
 
 // Listener Event to write password on click of "Start Quiz" button
-generateBtn.addEventListener("click", startQuiz);
+generateBtn.addEventListener("click", startGame);
